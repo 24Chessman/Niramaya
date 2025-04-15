0fbccb09b02d768ccd88a83090127df904ef4e1f
naitik <naitiksutariya.mscit22@vnsgu.ac.in>
2025-04-15 10:47:44 +0530

    forgot password added

--- backend/app.py
+++ backend/app.py
@@ -5,6 +5,7 @@ from routes.user_login_API import login_bp
 from routes.ask_AI_API import AI_bp
 from routes.chat_API import chat_bp
 from routes.member_API import member_bp
+from routes.password_reset_API import password_reset_bp
 
 # Initialize Flask app
 app = Flask(__name__)
@@ -16,6 +17,7 @@ app.register_blueprint(login_bp)
 app.register_blueprint(AI_bp)
 app.register_blueprint(chat_bp)
 app.register_blueprint(member_bp)
+app.register_blueprint(password_reset_bp )
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5000)
